06ed3aaec81041bee093c289e487d2d768f06b8d
jdann <juanddvalle20@gmail.com>
2021-02-06 20:28:59 -0600

    Se agregó activity Parameters, para data capture

---
+++ app/src/main/java/isv/zebra/com/zebracardprinter/Parameters.kt
@@ -0,0 +1,12 @@
+package isv.zebra.com.zebracardprinter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Parameters : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_parameters)
+    }
+}
